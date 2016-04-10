47e8cdf70675d5de853536e6364e96daf065c566
brickhz <15121041315@163.com>
2016-04-10 21:50:47 +0800

    v4 rabbish

--- app/src/main/java/com/example/thinkpad/hztest001/MainActivity.java
+++ app/src/main/java/com/example/thinkpad/hztest001/MainActivity.java
@@ -1,56 +1,141 @@
-package com.example.thinkpad.hztest001;
+package com.thisway.app_0001_leddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean ledon = false;
     private Button button = null;
-    private boolean ledon =false;
-    private CheckBox checkBoxLED1 = null;
-    private CheckBox checkBoxLED2 = null;
-    private CheckBox checkBoxLED3 = null;
-    private CheckBox checkBoxLED4 = null;
+    private CheckBox checkBoxLed1 = null;
+    private CheckBox checkBoxLed2 = null;
+    private CheckBox checkBoxLed3 = null;
+    private CheckBox checkBoxLed4 = null;
 
+    class MyButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            ledon = !ledon;
+            if (ledon) {
+                button.setText("ALL OFF");
+                checkBoxLed1.setChecked(true);
+                checkBoxLed2.setChecked(true);
+                checkBoxLed3.setChecked(true);
+                checkBoxLed4.setChecked(true);
+            }
+            else {
+                button.setText("ALL ON");
+                checkBoxLed1.setChecked(false);
+                checkBoxLed2.setChecked(false);
+                checkBoxLed3.setChecked(false);
+                checkBoxLed4.setChecked(false);
+            }
+        }
+    }
 
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
 
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.LED1:
+                if (checked) {
+                    // Put some meat on the sandwich
+                    Toast.makeText(getApplicationContext(), "LED1 on", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    // Remove the meat
+                    Toast.makeText(getApplicationContext(), "LED1 off", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.LED2:
+                if (checked) {
+                    // Put some meat on the sandwich
+                    Toast.makeText(getApplicationContext(), "LED2 on", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    // Remove the meat
+                    Toast.makeText(getApplicationContext(), "LED2 off", Toast.LENGTH_SHORT).show();
+                }
+                break;
 
+            case R.id.LED3:
+                if (checked) {
+                    // Put some meat on the sandwich
+                    Toast.makeText(getApplicationContext(), "LED3 on", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    // Remove the meat
+                    Toast.makeText(getApplicationContext(), "LED3 off", Toast.LENGTH_SHORT).show();
+                }
+                break;
 
+            case R.id.LED4:
+                if (checked) {
+                    // Put some meat on the sandwich
+                    Toast.makeText(getApplicationContext(), "LED4 on", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    // Remove the meat
+                    Toast.makeText(getApplicationContext(), "LED4 off", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            // TODO: Veggie sandwich
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button)findViewById(R.id.BUTTON);
-        checkBoxLED1 = (CheckBox) findViewById(R.id.LED1);
-        checkBoxLED2 = (CheckBox) findViewById(R.id.LED2);
-        checkBoxLED3 = (CheckBox) findViewById(R.id.LED3);
-        checkBoxLED4 = (CheckBox) findViewById(R.id.LED4);
 
+        button = (Button) findViewById(R.id.BUTTON);
 
+        checkBoxLed1 = (CheckBox) findViewById(R.id.LED1);
+        checkBoxLed2 = (CheckBox) findViewById(R.id.LED2);
+        checkBoxLed3 = (CheckBox) findViewById(R.id.LED3);
+        checkBoxLed4 = (CheckBox) findViewById(R.id.LED4);
+
+        button.setOnClickListener(new MyButtonListener());
+/*
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                // Perform action on click
                 ledon = !ledon;
-                if(ledon) {
+                if (ledon)
                     button.setText("ALL OFF");
-                    checkBoxLED1.setChecked(true);
-                    checkBoxLED2.setChecked(true);
-                    checkBoxLED3.setChecked(true);
-                    checkBoxLED4.setChecked(true);
-
-                }
-                    else {
+                else
                     button.setText("ALL ON");
-                    checkBoxLED1.setChecked(false);
-                    checkBoxLED2.setChecked(false);
-                    checkBoxLED3.setChecked(false);
-                    checkBoxLED4.setChecked(false);
-                }
-                //11111orm action on click
             }
         });
+*/
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
